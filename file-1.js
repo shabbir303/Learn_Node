781539ce2e2820fa6987d93655ac2ce03d2ebcdf
shabbir <shabbirhossain660@gmail.com>
2024-12-14 01:16:18 +0600

    utils file created

--- file-1.js
+++ file-1.js
@@ -1,2 +1,10 @@
 const a = 10;
-module.exports = a;
+// module.exports = a;
+const add = (param1 , param2)=> param1 + param2;
+
+module.exports = {
+    a,
+    add
+};
+
+console.log(module);
---
+++ main.js
@@ -0,0 +1,4 @@
+const {add, substract } = require("./utils")
+
+console.log(add(3,2));
+console.log(substract(9,2));
